750fb328cc275cda56b589da0b34328100f90639
megasw1 <snwskg@gmail.com>
2023-11-29 03:45:24 +0900

    2ck 수정(메인 페이지,지도 마커 추가)

--- app/src/main/java/com/example/medicationapp/PharmacyLocatorActivity.java
+++ app/src/main/java/com/example/medicationapp/PharmacyLocatorActivity.java
@@ -1,7 +1,17 @@
 package com.example.medicationapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -9,26 +19,114 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PharmacyLocatorActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest locationRequest;
+    private Location mCurrentLocation;
+    private final int LOCATION_PERMISSION_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharmacy_locator);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+
+        checkLocationPermission();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
+
+    private void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        if (location != null) {
+                            mCurrentLocation = location;
+                            updateLocationOnMap();
+                        }
+                    }
+                }
+            }, null);
+        }
+    }
+
+    private void updateLocationOnMap() {
+        LatLng seomyeonStation = new LatLng(35.1575, 129.0595);
+        mMap.addMarker(new MarkerOptions().position(seomyeonStation).title("서면역"));
+
+        // 지도 카메라를 서면역 위치로 이동
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seomyeonStation, 15));
+
+        // 공공 API를 사용하여 주변 약국을 검색하고 마커 추가하는 코드 필요
+    }
+
+   /* private void fetchPharmacies(LatLng location) {
+        String url = "공공 API 엔드포인트"; // API 엔드포인트 URL 설정
+
+        // Volley를 사용한 네트워크 요청
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONArray pharmaciesArray = response.getJSONArray("results");
+                        for (int i = 0; i < pharmaciesArray.length(); i++) {
+                            JSONObject pharmacy = pharmaciesArray.getJSONObject(i);
+                            double lat = pharmacy.getDouble("latitude");
+                            double lng = pharmacy.getDouble("longitude");
+                            String name = pharmacy.getString("name");
+
+                            LatLng pharmacyLocation = new LatLng(lat, lng);
+                            mMap.addMarker(new MarkerOptions().position(pharmacyLocation).title(name));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> error.printStackTrace());
+
+        Volley.newRequestQueue(this).add(jsonObjectRequest);
+    }*/
 }
